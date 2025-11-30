ffd7d119af16ccb92dfb19b0c7a6c279dd0eb49a
vladimir <fedyushinvovan@gmail.com>
2025-11-30 22:52:08 +0500

    draft

--- src/front/vite.config.js
+++ src/front/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   plugins: [react()],
   server: {
-    allowedHosts: ['charging-mardi-document-script.trycloudflare.com']
+    allowedHosts: ['microphone-chairman-identifying-conclusion.trycloudflare.com']
   },
 })
 
